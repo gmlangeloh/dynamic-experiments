5ab21af84aec34f71ba14e683e3010549194a511
Gabriel Mattos Langeloh <gmlangeloh@inf.ufrgs.br>
2019-01-21 01:20:36 -0200

    Adding a short script to deal with a bug in previous experiments

---
+++ fix_infs.py
@@ -0,0 +1,23 @@
+import glob
+import sys
+
+path = sys.argv[1]
+pattern = path + "*.test"
+files = glob.glob(pattern)
+empty_files = 0
+
+for filename in files:
+  with open(filename, "r+") as f:
+    contents = f.readlines()
+    instance_name = filename.split("/")[-1].split(".")[0]
+    if contents == []:
+      empty_files += 1
+      print instance_name, "empty"
+      continue
+    line0 = contents[0]
+    if line0.split()[0] == 'inf':
+      f.seek(0)
+      f.write(instance_name + " inf inf inf inf inf inf\n")
+      f.truncate()
+
+print "Empty files: ", empty_files
