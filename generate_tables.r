c523644dcb1e84be3e1d4e4a987b236629cfbdbe
Gabriel Mattos Langeloh <gmlangeloh@gmail.com>
2019-11-15 11:44:31 -0300

    Removing old version of files from the repository

--- generate_tables.r
+++
@@ -1,7 +0,0 @@
-library(xtable)
-
-files <- list.files(path="results", pattern="*.out", full.names=TRUE, recursive=FALSE)
-dummy <- lapply(files, function(x) {
-  t <- read.table(x, header=TRUE)
-  print(xtable(t, caption=x), include.rownames=F)
-})
